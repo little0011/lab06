aec0906788a07ffd247fc96123a18418e61ca5db
NatchaPetchsikul <natcha_pet@cmu.ac.th>
2022-08-18 21:51:39 +0700

    2.11

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="flashMessage" v-if="GStore.flashMessage">
     {{ GStore.flashMessage }}
   </div>
+  <h1>632115016 Natcha Petchsrikul</h1>
   <div id="nav">
     <router-link :to="{ name: 'EventList' }">Home</router-link> |
     <router-link :to="{ name: 'About' }">About</router-link>
